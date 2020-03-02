5237937adbdeabe38a85769babc4fa1dd141d10d
JunYoung <jhg080@naver.com>
2020-03-02 10:44:44 +0900

    World with Hatsune

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -4,5 +4,6 @@ public class PushPrint {
 		System.out.println("Add java class file push");
 		System.out.println("World with Miku");
 		System.out.println("world with 42");
+		System.out.println("World with Hatsune");
 	}
 }
